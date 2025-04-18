8eb1c6e97af61037d49c9b5fbda56d92dcf134f6
Mitesh <patelmiteshn@gmail.com>
2025-04-18 13:51:44 -0700

    added download and glibhi file

---
+++ src/download_model.py
@@ -0,0 +1,9 @@
+from huggingface_hub import hf_hub_download
+hf_hub_download(repo_id="Xiaojiu-Z/EasyControl", filename="models/canny.safetensors", local_dir="/ssd4TB/huggingface/hub/")
+hf_hub_download(repo_id="Xiaojiu-Z/EasyControl", filename="models/depth.safetensors", local_dir="/ssd4TB/huggingface/hub/")
+hf_hub_download(repo_id="Xiaojiu-Z/EasyControl", filename="models/hedsketch.safetensors", local_dir="/ssd4TB/huggingface/hub/")
+hf_hub_download(repo_id="Xiaojiu-Z/EasyControl", filename="models/inpainting.safetensors", local_dir="/ssd4TB/huggingface/hub/")
+hf_hub_download(repo_id="Xiaojiu-Z/EasyControl", filename="models/pose.safetensors", local_dir="/ssd4TB/huggingface/hub/")
+hf_hub_download(repo_id="Xiaojiu-Z/EasyControl", filename="models/seg.safetensors", local_dir="/ssd4TB/huggingface/hub/")
+hf_hub_download(repo_id="Xiaojiu-Z/EasyControl", filename="models/subject.safetensors", local_dir="/ssd4TB/huggingface/hub/")
+hf_hub_download(repo_id="Xiaojiu-Z/EasyControl", filename="models/Ghibli.safetensors", local_dir="/ssd4TB/huggingface/hub/")
